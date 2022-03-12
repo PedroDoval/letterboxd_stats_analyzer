19bc7602bc1d86c1e2dffecc3ae960c5ad2ca7d2
palonso <palonso@gradiant.org>
2022-03-12 13:56:38 +0100

    update front

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a tool for analyze the data of your Telegram chat history and represent it with plots.
+This is a tool for analyze the data of your Letterboxd history and represent it with plots.
 
 # Installation
 `pip install -r requirements`
--- plot_utils.py
+++ plot_utils.py
@@ -15,9 +15,9 @@ def plot_bars_messages(count_messages, title, output_dir, filename=None):
     plt.xlabel("Mes")
     plt.ylabel("Nº películas")
     plt.xticks(rotation=65, ha="right")
-    plt.title(title)
-    plt.bar(names, values)
-    fig.savefig(os.path.join(output_dir, filename + ".png"), transparent=True)
+    plt.title(title, fontweight="bold")
+    plt.bar(names, values, color='#00e054', edgecolor='black')
+    fig.savefig(os.path.join(output_dir, filename + ".png"), transparent=True, bbox_inches='tight')
     #plt.show()
 
 
@@ -28,9 +28,10 @@ def plot_pie_messages(count_messages, title, output_dir, filename=None):
     names = count_messages.keys()
 
     fig = plt.figure()
-    plt.title(title)
-    plt.pie(values, labels=names)
-    fig.savefig(os.path.join(output_dir, filename + ".png"), transparent=True)
+    plt.title(title, fontweight="bold")
+    colors = ['#00e054', '#ff8000']
+    plt.pie(values, labels=names, colors=colors)#, edgecolor='black')
+    fig.savefig(os.path.join(output_dir, filename + ".png"), transparent=True, bbox_inches='tight')
     #plt.show()
 
 
@@ -51,7 +52,7 @@ def plot_day_and_hour(df, title, user, output_dir, filename=None):
     )
     plt.xticks(rotation=50, fontsize=8)
     plt.yticks(fontsize=8)
-    fig.savefig(os.path.join(output_dir, "plot_day_and_hour_" + user + ".png"), transparent=True)
+    fig.savefig(os.path.join(output_dir, "plot_day_and_hour_" + user + ".png"), transparent=True, bbox_inches='tight')
     # colorbar = ax.collections[0].colorbar
     # colorbar.set_ticks([0, 2.2])
     # plt.show()
@@ -64,9 +65,10 @@ def plot_pie_messages(count, title, output_dir, filename=None):
     names = count.keys()
 
     fig = plt.figure()
-    plt.title(title)
-    plt.pie(values, labels=names, autopct=lambda p: '{:.0f}'.format(p * sum(values) / 100), shadow=True)
-    fig.savefig(os.path.join(output_dir, filename + ".png"), transparent=True)
+    plt.title(title, fontweight="bold")
+    colors = ['#00e054', '#ff8000']
+    plt.pie(values, labels=names, colors=colors, autopct=lambda p: '{:.0f}'.format(p * sum(values) / 100), shadow=True)
+    fig.savefig(os.path.join(output_dir, filename + ".png"), transparent=True, bbox_inches='tight')
     #plt.show()
 
 
@@ -76,14 +78,14 @@ def plot_lists_pies(lists, config, filename=None):
 
 
     fig = plt.figure()
-    fig.suptitle('Listas del usuario')
+    fig.suptitle('Listas y % de películas vistas', fontweight="bold")
     # 2 rows 2 columns
     rows = int(len(lists)/2)
     if len(lists)%2 == 1: rows += 1
     i=0
     j=0
     index = 0
-    colors=("blue", "orange")
+    colors = ['#00e054', '#ff8000']
     for lista in lists:
         values = [lista["num_watched"], lista["size"]-lista["num_watched"]]
         # first row, first column
@@ -101,7 +103,7 @@ def plot_lists_pies(lists, config, filename=None):
 
     patches, texts = ax1.pie(values, colors=colors, startangle=90)  # use this plot to show the legend
     plt.legend(patches, ['seen', 'unseen'], bbox_to_anchor=(2.3, 2), prop={'size': 10}, loc='best')  # show the legend defined in labels
-    fig.savefig(os.path.join(config["output_dir"], filename + ".png"), transparent=True)
+    fig.savefig(os.path.join(config["output_dir"], filename + ".png"), transparent=True, bbox_inches='tight')
     #plt.show()
 
 def equalize_size_list(lista, maxsize):
@@ -118,15 +120,16 @@ def plot_table(max_films, max_rate, min_films, min_rate, output_dir, filename):
     data = np.array([list(max_films), list(min_films)]).transpose()
     fig, ax = plt.subplots()
     ax.set_axis_off()
-    plt.table(cellText=data,
+    table = plt.table(cellText=data,
         rowLabels=["" for i in range(0,max_len)],
-        colColours=["palegreen"] * 2,
         colLabels=["Highest score ("+str(max_rate)+"★)","Worst score ("+str(min_rate)+"★)"],
         loc='center',
         cellLoc ='center',
     )
-    ax.set_title("Movies with higher or smaller rating")
-    fig.savefig(os.path.join(output_dir, filename + ".png"), transparent=True)
+    table[(0,0)].set_facecolor("#00e054")
+    table[(0,1)].set_facecolor("#00e054")
+    #ax.set_title("Movies with higher or smaller rating")
+    fig.savefig(os.path.join(output_dir, filename + ".png"), transparent=True)# bbox_inches='tight')
     #plt.show()
 
 def wordcloud(texts, output_dir):
